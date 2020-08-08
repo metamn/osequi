789f3fea7fbff4e3c389aaeb31c99684824cd24d
metamn <bartus.csongor@gmail.com>
2020-08-09 00:40:36 +0300

    Updating copy

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,23 @@ const Home = () => {
         <li>An altruistic web studio.</li>
         <li>If it's greater than you, we are in.</li>
         <li>
-          We've accumulated decades of experience in delivering messages. now we
+          We've accumulated decades of experience in delivering messages. Mow we
           put that expertise in the service of the human mankind.
         </li>
         <li>
-          It's your turn. come up with things that matter, and we'll invest
+          It's your turn. Come up with things that matter, and we'll invest
           alongside.
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          We stand on the shoulders of giants. At this moment that's Facebook's
+          React and Amazon's Web Services.
+        </li>
+        <li>
+          Concerns? They are both open source. Millions for free. For us. Let's
+          harvest it and take all pills.
         </li>
       </ul>
     </>
