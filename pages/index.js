f5390b125aa8ad02aabedfb822502537100bb3b8
metamn <bartus.csongor@gmail.com>
2020-08-09 00:09:56 +0300

    Adding mission

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,16 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <p>Os Equi</p>
+      <p>an altruistic web studio.</p>
+      <p>if it's greater than you, we are in.</p>
+      <p>
+        we've accumulated decades of experience in delivering messages. now we
+        put that expertise in the service of the human mankind.
+      </p>
+      <p>
+        it's your turn. come up with things that matter, and we'll invest
+        alongside.
+      </p>
     </>
   );
 };
