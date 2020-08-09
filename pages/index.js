a7066e37959192aa46043f53be16b62f5c48b012
metamn <bartus.csongor@gmail.com>
2020-08-09 10:54:01 +0300

    Updating copy

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,34 @@ const Home = () => {
       <ul>
         <li>A Design System for all. --> Open source</li>
         <li>A take on React best practices. --> Open source</li>
+        <li>inu.ro, a photographer's portfolio --> Open source</li>
+        <li>A take on WordPress best practices. --> Open source</li>
+        <li>
+          Log Lolla, a WordPress theme to capture knowledge. --> Open source,
+          featured in WordPress Theme Store
+        </li>
+        <li>
+          Gust, a postmodern dictionary. --> Open source, featured on Brutalist
+          Websites
+        </li>
+        <li>
+          Yet Another Color Scheme Designer + Color Theory --> Open source
+        </li>
+        <li>
+          Gravity point. Find the centre of gravity of a webpage --> Open source
+        </li>
+      </ul>
+
+      <p>Resources:</p>
+      <ul>
+        <li>Fonts for open source projects.</li>
+        <li>Ontology, taxonomy, choreography.</li>
+        <li>Bauhaus to emoji: A call for a new typography.</li>
+        <li>Consistently eventual.</li>
+        <li>The journey of a blog post in 2020.</li>
+        <li>Rethinking The Web, The Internet, And Our Roles Within.</li>
+        <li>Devices as designers.</li>
+        <li>Delivering the message.</li>
       </ul>
 
       <p>News:</p>
