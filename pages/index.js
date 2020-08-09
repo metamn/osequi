e6f0edd3cd25ec73eacb75a8323974e7685b1286
metamn <bartus.csongor@gmail.com>
2020-08-09 10:16:25 +0300

    Updting copy

--- pages/index.js
+++ pages/index.js
@@ -8,55 +8,25 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <p>Os Equi</p>
-
-      <ul>
-        <li>An altruistic web studio.</li>
-        <li>If it's greater than you, we are in.</li>
-        <li>
-          We've accumulated decades of experience in delivering messages. Now we
-          put that expertise in the service of the human mankind.
-        </li>
-        <li>
-          It's your turn. Come up with things that matter, and we'll invest
-          alongside.
-        </li>
-      </ul>
-
-      <p>Approach:</p>
-      <ul>
-        <li>
-          We stand on the shoulders of giants. At this moment that's Facebook's
-          React and Amazon's Web Services (AWS).
-        </li>
-        <li>
-          Concerns? React is open source. Millions for free. For us. AWS is the
-          low cost infrastructure service.
-        </li>
-        <li>
-          More concerns? Yes, they might be evil in business now. Tomorrow
-          technology will triumph them all. Information, like us, wants to be
-          free.
-        </li>
-      </ul>
-
-      <p>Sustainability:</p>
-      <ul>
-        <li>We are working on, and invest in altruistic projects first.</li>
-        <li>
-          When needed we take commercial projects, for profit, and convert the
-          learnings in future Os Equi projects.
-        </li>
-      </ul>
+      <p>An altruistic web studio.</p>
+      <p>
+        If it's greater than you, we are in. Come up with ideas that matter, and
+        we'll invest alongside.
+      </p>
+      <p>
+        We've accumulated experience in delivering messages. Now we put that
+        expertise in the service of the human mankind.
+      </p>
 
       <p>Projects:</p>
-      <ol>
+      <ul>
         <li>A Design System for all. --> Open source</li>
-      </ol>
+      </ul>
 
       <p>News:</p>
-      <ol>
+      <ul>
         <li>Energy-saving design for Os Equi. --> Open source</li>
-      </ol>
+      </ul>
     </>
   );
 };
